2e5d335c6f5c4c2696323f1940f8f90f3dc56684
Santiago Duarte <santiiagoduarte@hotmail.com>
2022-11-16 00:22:54 -0500

    Events & List

--- App.js
+++ App.js
@@ -1,61 +1,109 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
+import { useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+  TouchableOpacity,
+  FlatList,
+} from "react-native";
+
+import Modal from "./components/Modal";
 
 export default function App() {
+  const [textItem, setTextItem] = useState("");
+  const [list, setList] = useState([]);
+
+  const [modalVisible, setModalVisible] = useState(false);
+
+  const [itemSelected, setItemSelected] = useState({});
+
+  const onHandleChange = (t) => setTextItem(t);
+
+  const addItem = () => {
+    setList((currentState) => [
+      ...currentState,
+      { id: Math.random().toString(), value: textItem },
+    ]);
+    setTextItem("");
+    console.log("");
+  };
+
+  const selectedItem = (id) => {
+    console.log(id);
+    setItemSelected(list.find((item) => item.id === id));
+    setModalVisible(true);
+  };
+
+  const deleteItem = () => {
+    console.log(itemSelected);
+    setList((currentState) =>
+      currentState.filter((item) => item.id !== itemSelected.id)
+    );
+    setItemSelected({});
+    setModalVisible(false);
+  };
+
+  const renderItem = ({ item }) => (
+    <TouchableOpacity onPress={() => selectedItem(item.id)}>
+      <Text>{item.value}</Text>
+    </TouchableOpacity>
+  );
+
   return (
-    <View style={{flex: 1, backgroundColor: '#7DE5ED'}}>
-      <View style={{flex: 1, backgroundColor: '#7DE5ED'}}>
-        <Text style={{}}></Text>
+    <View style={styles.container}>
+      <Text style={{ fontSize: 30 }}>FeelGood App</Text>
+      <View style={styles.inputcontainer}>
+        <TextInput
+          placeholder="New Service"
+          placeholderTextColor="black"
+          style={styles.inputStyle}
+          value={textItem}
+          onChangeText={onHandleChange}
+        />
+        <TouchableOpacity style={styles.button} onPress={addItem}>
+          <Text> Add </Text>
+        </TouchableOpacity>
       </View>
-
-      <View style={styles.itemContainer}>
-        <TextInput placeholder='List item' style={styles.inputItem}/> 
-        <Button title='ADD' style={styles.buttonStyle}/>
-        <TextInput placeholder='List item' style={styles.inputItem}/> 
-        <Button title='ADD' style={styles.buttonStyle}/>
-        <TextInput placeholder='List item' style={styles.inputItem}/> 
-        <Button title='ADD' style={styles.buttonStyle}/>
-        <TextInput placeholder='List item' style={styles.inputItem}/> 
-        <Button title='ADD' style={styles.buttonStyle}/>
+      <View>
+        <FlatList
+          data={list}
+          renderItem={renderItem}
+          keyExtractor={(item) => item.id}
+        />
       </View>
-
-      <View style={{flex: 1, backgroundColor: '#7DE5ED'}}></View>
+      <Modal isVisible={modalVisible} actionDeleteItem={deleteItem} />
     </View>
-
-
-    // <View style={styles.container}>
-    //     <View style={styles.itemContainer}>
-
-    //     </View>
-    //     <View>
-    //     </View> 
-    // </View>
   );
 }
 
-
 const styles = StyleSheet.create({
   container: {
-    flex: 1, 
-    padding: 30, 
-    marginTop: 50,
+    flex: 1,
+    backgroundColor: "#7DE5ED",
+    alignItems: "center",
+    paddingTop: 100,
   },
-  itemContainer: {
-    flex: 6, 
-    backgroundColor: 'white',
-    flexDirection: 'column', 
-    justifyContent: 'center', 
-    alignItems: 'center'
+  inputcontainer: {
+    marginTop: 30,
+    flexDirection: "row",
+    justifyContent: "space-around",
+    alignItems: "center",
+    width: "100%",
+    paddingHorizontal: 30,
   },
-  inputItem: {
-    margin: 20,
-    borderBottomColor: 'black',
+  inputStyle: {
+    borderBottomColor: "black",
     borderBottomWidth: 1,
-    width: 200,
+    width: 250,
   },
-  buttonStyle: {
-    borderStyle: 'solid',
-    // borderRadius: 3,
-    backgroundColor: '#5837D0'
-  }
+  button: {
+    backgroundColor: "#5837D0",
+    height: 35,
+    width: 45,
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 15,
+    fontColor: "white"
+  },
 });
---
+++ AppTest.js
@@ -0,0 +1,61 @@
+// import { StatusBar } from 'expo-status-bar';
+// import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
+
+// export default function App() {
+//   return (
+//     <View style={{flex: 1, backgroundColor: '#7DE5ED'}}>
+//       <View style={{flex: 1, backgroundColor: '#7DE5ED'}}>
+//         <Text style={{}}></Text>
+//       </View>
+
+//       <View style={styles.itemContainer}>
+//         <TextInput placeholder='List item' style={styles.inputItem}/> 
+//         <Button title='ADD' style={styles.buttonStyle}/>
+//         <TextInput placeholder='List item' style={styles.inputItem}/> 
+//         <Button title='ADD' style={styles.buttonStyle}/>
+//         <TextInput placeholder='List item' style={styles.inputItem}/> 
+//         <Button title='ADD' style={styles.buttonStyle}/>
+//         <TextInput placeholder='List item' style={styles.inputItem}/> 
+//         <Button title='ADD' style={styles.buttonStyle}/>
+//       </View>
+
+//       <View style={{flex: 1, backgroundColor: '#7DE5ED'}}></View>
+//     </View>
+
+
+//     // <View style={styles.container}>
+//     //     <View style={styles.itemContainer}>
+
+//     //     </View>
+//     //     <View>
+//     //     </View> 
+//     // </View>
+//   );
+// }
+
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1, 
+//     padding: 30, 
+//     marginTop: 50,
+//   },
+//   itemContainer: {
+//     flex: 6, 
+//     backgroundColor: 'white',
+//     flexDirection: 'column', 
+//     justifyContent: 'center', 
+//     alignItems: 'center'
+//   },
+//   inputItem: {
+//     margin: 20,
+//     borderBottomColor: 'black',
+//     borderBottomWidth: 1,
+//     width: 200,
+//   },
+//   buttonStyle: {
+//     borderStyle: 'solid',
+//     // borderRadius: 3,
+//     backgroundColor: '#5837D0'
+//   }
+// });
